caca9764622a11257af94227c71a39c8327782ef
nobuti <buti@nobuti.com>
2017-02-18 10:30:44 +0100

    Deleted parallax.
    Added scroll observer plugin.

--- _javascripts/main.js
+++ _javascripts/main.js
@@ -1,14 +1,13 @@
 var $ = require('jquery');
 window.jQuery = $;
 
-require('jquery-parallax.js');
-
 $(function () {
   require('./vendor/jquery.tablehover')($);
   require('./components/dropdown').init();
   require('./components/table').init();
   require('./components/search').init();
   require('./components/header').init();
+  require('./components/scroll-observer').init();
 
   var highlight = require('./components/highlight');
   highlight();
