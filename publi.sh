36e11cb44e1475a534526aef66cdfea9db191992
nobuti <buti@nobuti.com>
2017-03-07 22:17:43 +0100

    Tue Mar  7 22:17:43 CET 2017

--- publi.sh
+++ publi.sh
@@ -3,6 +3,7 @@ git checkout site
 git merge master
 rm -rf _site/
 bundle exec jekyll build --baseurl /cuentas-anuales
+gulp search
 git add --all
 git commit -m "`date`"
 git push origin site --force
